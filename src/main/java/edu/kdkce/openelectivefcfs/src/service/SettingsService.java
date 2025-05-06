db6ff49fbd79530ac614466c59f5119ba0c9a947
tejasumate118 <tejasumate118@gmail.com>
2025-05-06 08:15:22 +0530

    OpenElectiveFCFS

---
+++ src/main/java/edu/kdkce/openelectivefcfs/src/service/SettingsService.java
@@ -0,0 +1,41 @@
+package edu.kdkce.openelectivefcfs.src.service;
+
+import edu.kdkce.openelectivefcfs.src.dto.ElectiveTimeResponse;
+import edu.kdkce.openelectivefcfs.src.dto.ElectiveTimeUpdateRequest;
+import edu.kdkce.openelectivefcfs.src.model.Settings;
+import edu.kdkce.openelectivefcfs.src.repository.SettingsRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+
+@Service
+public class SettingsService {
+
+    private final SettingsRepository settingsRepository;
+
+    public SettingsService(SettingsRepository settingsRepository) {
+        this.settingsRepository = settingsRepository;
+    }
+
+    public ElectiveTimeResponse getElectiveTime() {
+        Settings settings = settingsRepository.findById(1L).orElseThrow(() -> new RuntimeException("Settings not found"));
+
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        boolean isActive = now.isAfter(settings.getElectiveOpeningTime()) && now.isBefore(settings.getElectiveClosingTime());
+
+        return new ElectiveTimeResponse(
+                settings.getElectiveOpeningTime(),
+                settings.getElectiveClosingTime(),
+                isActive
+        );
+    }
+
+    public void updateElectiveTime(ElectiveTimeUpdateRequest request) {
+        Settings settings = settingsRepository.findById(1L).orElse(new Settings());
+        settings.setElectiveOpeningTime(request.allocationStartDate());
+        settings.setElectiveClosingTime(request.allocationEndDate());
+        settingsRepository.save(settings);
+    }
+}
